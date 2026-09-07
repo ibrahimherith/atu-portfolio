8599b79f3c12115da3ca0930f7cf67cd22a39b45
Atu Tweve <atutweve64@gmail.com>
2026-09-08 01:12:58 +0300

    added details in skills and projects page

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,22 +1,51 @@
 import Navbar from "../components/Navbar";
 
 const ProjectPage = () => {
-    return ( 
-         <>
-      <section
-         className="items-center min-h-screen pt-4"
-      >
+  return ( 
+    <>
+      <Navbar />
+      <section className="h-screen items-center pt-15 mx-auto">
 
-        <Navbar />
+        <h1 className="text-center text-5xl font-bold">My Projects</h1>
 
-        <h1 className="text-center text-5xl font-bold pt-5">My Projects</h1>
+        <div className="min-h-100 flex justify-center items-center overflow-hidden">
+        <div className="grid grid-cols-2 grid-rows-2 gap-10 text-center">
+                    
+          <div className="block px-2 py-2 hover:bg-amber-100">
+            <div className="">
+              <img src="dev 2.jpg" className="w-50 rounded-md"/>
+            </div>
+            <h3 className="text-2xl">Project 1</h3>
+          </div>
 
-        <p className="mx-auto mt-10 max-w-2xl text-center">
-          I am a Frontend Developer and Product Specialist.
-        </p>
+          <div className="block px-2 py-2 hover:bg-amber-100">
+            <div className="">
+              <img src="dev 2.jpg" className="w-50 rounded-md"/>
+            </div>
+            <h3 className="text-2xl">Project 2</h3>
+          </div>
+
+          <div className="block px-2 py-2 hover:bg-amber-100">
+            <div className="">
+              <img src="dev 2.jpg" className="w-50 rounded-md"/>
+            </div>
+            <h3 className="text-2xl">Project 3</h3>
+          </div> 
+
+          <div className="block px-2 py-2 hover:bg-amber-100">
+            <div className="">
+              <img src="dev 2.jpg" className="w-50 rounded-md"/>
+            </div>                  
+            <h3 className="text-2xl">Project 4</h3>
+          </div>
+
+          </div>
+
+        </div>
+
       </section>
     </>
-     );
+  );
 }
  
 export default ProjectPage;
--- src/Pages/SkillsPage.jsx
+++ src/Pages/SkillsPage.jsx
@@ -2,43 +2,44 @@ import Navbar from "../components/Navbar";
 const SkillsPage = () => {
     return ( 
         <>
+            <Navbar />
             <section
-                className="items-center pt-4"
+                className="h-screen items-center pt-15 mx-auto"
             >
-
-                <Navbar />
-
                 <h1 className="text-center text-5xl font-bold">My Skills</h1>
 
-                <div className="grid grid-row-3 mx-auto mt-10 max-w-2xl text-center">
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
-                    </div>
+                <div className="min-h-100 flex justify-center items-center overflow-hidden">
+                    <div className="grid grid-cols-3 grid-rows-2 gap-10">
+                    
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">HTML</h3>
+                            <p className="">4 years Experience</p>
+                        </div>
 
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
-                    </div>
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">CSS</h3>
+                            <p className="">4 years Experience</p>
+                        </div>
 
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
-                    </div>
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">JAVASCRIPT</h3>
+                            <p className="">4 years Experience</p>
+                        </div> 
 
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
-                    </div>
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">REACT</h3>
+                            <p className="">4 years Experience</p>
+                        </div>
 
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
-                    </div>
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">TAILWIND</h3>
+                            <p className="">4 years Experience</p>
+                        </div>
 
-                    <div className="bg-amber-300 flex">
-                        <h3 className=""></h3>
-                        <p className=""></p>
+                        <div className="block px-2 py-2">
+                            <h3 className="text-2xl">FIGMA</h3>
+                            <p className="">4 years Experience</p>
+                        </div>
                     </div>
                 </div>
             </section>
