1666a384d66993286f2fc167e4702cebb149ae6e
Ibrahim Herith <ibrahimherith19@gmail.com>
2026-09-10 12:38:07 +0300

    Updates on navbar items to use react router Link instead of anchor tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,35 @@
 import { useState } from "react";
 import { LuMenu } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
       <div className="mx-auto bg-cyan-600 w-full text-white px-8 py-2 fixed z-9999 top-0">
-
         <div className="flex justify-end md:hidden">
-          <LuMenu 
-            classname= "text-3xl" 
-            onClick={() => setIsMenuOpen(prev => !prev)}
+          <LuMenu
+            classname="text-3xl"
+            onClick={() => setIsMenuOpen((prev) => !prev)}
           />
         </div>
 
         {/* desktop view */}
         <ul className="md:flex justify-end gap-20 hidden ">
-
-          <li className="hover:text-amber-300"> 
-            <a href="/">HOME</a>
+          <li className="hover:text-amber-300">
+            <Link to="/">HOME</Link>
           </li>
           <li className="hover:text-amber-300">
-            <a href="/about">ABOUT</a>
+            <Link to="/about">ABOUT</Link>
           </li>
           <li className="hover:text-amber-300">
-            <a href="/project">PROJECTS</a>
+            <Link to="/project">PROJECTS</Link>
           </li>
           <li className="hover:text-amber-300">
-            <a href="/skill">SKILLS</a>
+            <Link to="/skill">SKILLS</Link>
           </li>
           <li className="hover:text-amber-300">
-            <a href="/contact">CONTACT</a>
+            <Link to="/contact">CONTACT</Link>
           </li>
         </ul>
 
@@ -46,25 +45,24 @@ const Navbar = () => {
           <div className="bg-cyan-500 absolute top-8 right-0 w-1/2 md:hidden">
             <ul className="w-full justify-items-center py-4">
               <li className="hover:text-amber-300">
-                <a href="/">HOME</a>
+                <Link to="/">HOME</Link>
               </li>
               <li className="hover:text-amber-300">
-                <a href="/about">ABOUT</a>
+                <Link to="/about">ABOUT</Link>
               </li>
               <li className="hover:text-amber-300">
-                <a href="/project">PROJECTS</a>
+                <Link to="/project">PROJECTS</Link>
               </li>
               <li className="hover:text-amber-300">
-                <a href="/skill">SKILLS</a>
+                <Link to="/skill">SKILLS</Link>
               </li>
               <li className="hover:text-amber-300">
-                <a href="/contact">CONTACT</a>
+                <Link to="/contact">CONTACT</Link>
               </li>
             </ul>
           </div>
-      )}
+        )}
       </div>
-
     </>
   );
 };
