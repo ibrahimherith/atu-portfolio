5dafefa53c66135455c146118830e0dc688bec33
Atu Tweve <atutweve64@gmail.com>
2026-09-15 15:21:25 +0300

    implemented navigation icons for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,10 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <div className="mx-auto bg-cyan-600 w-full text-white px-8 py-2 fixed z-9999 top-0">
-        <div className="flex justify-between md:hidden">
-          <div className="w-1/4 rounded">
-            {/* <img src="logo1.png" /> */}
-          </div>
-          <LuMenu
-            classname="text-3xl"
-            onClick={() => setIsMenuOpen((prev) => !prev)}
-          />
+      <div className="flex justify-between items-center mx-auto bg-cyan-600 w-full text-white px-8 py-2 fixed z-9999 top-0">
+
+        <div className="bg-white/80 w-10 md:w-20 rounded-full">
+          <img src="logo1.png" className="w-full h-auto"/>
         </div>
 
         {/* desktop view */}
@@ -22,10 +17,6 @@ const Navbar = () => {
           <li className="hover:text-amber-300">
             <Link to="/">HOME</Link>
           </li>
-
-          {/* <li className="hover:text-amber-300">
-            <Link to="/about">ABOUT</Link>
-          </li> */}
 
           <li className="hover:text-amber-300">
             <Link to="/project">PROJECTS</Link>
@@ -38,36 +29,46 @@ const Navbar = () => {
           </li>
         </ul>
 
-        {isMenuOpen && (
-          <div
-            className="fixed inset-0 bg-black/40 backdrop-blur-sm lg:hidden"
-          />
-        )}
-
-        {/* mobile view */}
-        {isMenuOpen && (
-          <div className="bg-cyan-500 flex absolute top-8 right-0 w-1/2 md:hidden">
-            <ul className="w-full justify-items-center py-4">
-              <li className="hover:text-amber-300">
-                <Link to="/">HOME</Link>
-              </li>
-              <li className="hover:text-amber-300">
-                <Link to="/project">PROJECTS</Link>
-              </li>
-              <li className="hover:text-amber-300">
-                <Link to="/skill">SKILLS</Link>
-              </li>
-              <li className="hover:text-amber-300">
-                <Link to="/contact">CONTACT</Link>
-              </li>
-            </ul>
-
-            <div>
-              <LuX onClick={() => setIsMenuOpen(false)}/>
-            </div>
-          </div>
-        )}
+        <button className="md:hidden text-3xl"
+          onClick={() => setIsMenuOpen((prev) => !prev)}>
+          {isMenuOpen ? <LuMenu /> : <LuX />}
+        </button>
+        
       </div>
+
+      {isMenuOpen && (
+        <div
+          className="fixed inset-0 bg-black/40 backdrop-blur-sm lg:hidden"
+        />
+      )}
+      
+      {/* mobile view */}
+      {isMenuOpen && (
+        <div className="bg-cyan-500 flex absolute top-8 right-0 w-1/2 md:hidden">
+          <ul className="w-full justify-items-center py-4">
+            <li className="hover:text-amber-300">
+              <Link to="/">HOME</Link>
+            </li>
+
+            <li className="hover:text-amber-300">
+              <Link to="/project">PROJECTS</Link>
+            </li>
+
+            <li className="hover:text-amber-300">
+              <Link to="/skill">SKILLS</Link>
+            </li>
+
+            <li className="hover:text-amber-300">
+              <Link to="/contact">CONTACT</Link>
+            </li>
+          </ul>
+
+          {/* <div>
+            <LuX onClick={() => setIsMenuOpen(false)}/>
+          </div> */}
+        </div>
+      )}
+
     </>
   );
 };
